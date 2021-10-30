4f155baa9a475fccfb628b2f13e8ada58ee4c5fc
gus-martins <gustavomartins9029@gmail.com>
2021-10-30 13:03:31 -0300

    Bloco III

---
+++ estressados/bloco III/alteracao.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "testador.hpp"
+
+using namespace std;
+
+vector<int> remove(vector<int>& vet, int position) {
+
+    vet.erase(vet.begin() + position);
+
+    return vet;
+
+}
+
+vector<int> insert(vector<int>& vet, int position, int value) {
+    
+    vet.insert(vet.begin() + position, value);
+
+    return vet;
+}
+
+vector<int> dance(vector<int> vet) {
+
+    for (size_t i = 0; i < vet.size() - 1; i++)
+    {
+        if (abs(vet[i]) == abs(vet[i + 1]))
+        {
+            vet.erase(vet.begin() + i);
+            vet.erase(vet.begin() + (i+1));
+        }
+        
+    }
+    // for (size_t i = 0; i < vet.size(); i++)
+    // {
+    //     if (abs(vet[i]) == abs(vet[i + 1])) 
+    //     {
+    //             dance(vet);
+    //     }
+    // }
+        
+
+    return vet;
+}
+
+void dance_test() {
+    cout << "dance_test" << endl;
+    testar(dance({1, -1, 2, 3, -3, 2, 4, 5, 6, 10, 50, -50, -10}), {2, 2, 4, 5, 6, 10, -10});
+}
+
+
+int main () {
+
+    vector<int> vet = {0, 1, 2, 3, 4, 5, 6, 7};
+
+    remove(vet, 2);
+    cout << "vetor apos remover\n" << vet << endl;
+
+    insert(vet, 2, 10);
+    cout << "vetor apos inserir\n" << vet << endl;
+
+    dance_test();
+}
